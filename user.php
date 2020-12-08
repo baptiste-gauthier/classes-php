3960d7edde0c7e2d9615e6792eecdf3094f53cd0
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2020-12-08 14:38:12 +0100

    fonction update

--- user.php
+++ user.php
@@ -8,14 +8,16 @@
         public $email ; 
         public $firstname ; 
         public $lastname ; 
+        public $connexion ; 
 
         public function __construct($login,$password,$email,$firstname,$lastname){
-            // $this->id = $id ; 
+           
             $this->login = $login ;
             $this->password = $password ; 
             $this->email = $email ; 
             $this->firstname = $firstname ; 
             $this->lastname = $lastname ; 
+            $this->connexion = 0 ;
         }
 
         public function register() {
@@ -25,25 +27,35 @@
                 printf("Échec de la connexion : %s\n", mysqli_connect_error());
                 exit();
             }
+            
+            $requete = "SELECT login FROM utilisateurs WHERE login = '$this->login' ;" ;
+            $query = mysqli_query($db,$requete) ;
 
-            $requete = "INSERT INTO utilisateurs(login,password,email,firstname,lastname) 
-                            VALUES ('$this->login', '$this->password', '$this->email', '$this->firstname','$this->lastname'
-            );";
+            var_dump($query) ; 
 
-            $query = mysqli_query($db,$requete) ; 
+            if($query->num_rows == 0)
+            {
+                $requete = "INSERT INTO utilisateurs(login,password,email,firstname,lastname) 
+                                VALUES ('$this->login', '$this->password', '$this->email', '$this->firstname','$this->lastname'
+                );";
+    
+                $query = mysqli_query($db,$requete) ; 
+    
+                var_dump($query); 
+                var_dump($requete) ; 
+    
+                echo 'utilisateur ajouté !' ; 
+    
+                $result = [$this->login,$this->password,$this->email,$this->firstname,$this->lastname] ; 
+                var_dump($result);
+                return $result ; 
+            }
+            else
+            {
+                echo 'ce login est déjà utiliser' ; 
+            }
 
-            var_dump($query); 
-            var_dump($requete) ; 
 
-            echo 'utilisateur ajouté !' ; 
-
-            $sql = "SELECT * FROM utilisateurs ORDER BY id DESC" ;
-            $exec = mysqli_query($db,$sql) ; 
-
-            $result = mysqli_fetch_all($exec) ; 
-            var_dump($result[0]) ;
-
-            return $result[0] ; 
 
             //  Partie PDO 
 
@@ -67,22 +79,16 @@
                             WHERE login = '$this->login' AND password = '$this->password'" ;
 
             $query = mysqli_query($db,$requete) ;
+            var_dump($query) ; 
             
-            if($query)
+            if($query->num_rows == 1)
             {
                 echo 'Bravo vous êtes connecté' ; 
+                $this->connexion = 1 ; 
+                var_dump($this->connexion) ; 
 
-                $resultat = mysqli_fetch_assoc($query) ; 
+                return [$this->login, $this->password] ; 
 
-                var_dump($resultat);
-                $_SESSION['login'] = $this->login ; 
-                $_SESSION['pass'] = $this->password ; 
-                $_SESSION['id'] = $resultat['id'] ; 
-                $_SESSION['email'] = $resultat['email'] ; 
-                $_SESSION['firstname'] = $resultat['firstname'];
-                $_SESSION['lastname'] = $resultat['lastname'] ; 
-
-                return $resultat ; 
             }
             else
             {
@@ -92,49 +98,86 @@
         }
 
         public function disconnect(){
-            session_destroy() ;
 
+            unset($this->login) ;
+            unset($this->password) ; 
+            unset($this->email) ; 
+            unset($this->firstname) ;  
+            unset($this->lastname) ;  
+            
             echo 'Vous avez été déconnecté' ; 
         }
 
         public function delete(){
-            session_destroy();
 
+            
             $db = mysqli_connect("localhost","root","","classes") ; 
-
+            
             if (mysqli_connect_errno()) {
                 printf("Échec de la connexion : %s\n", mysqli_connect_error());
                 exit();
             }
-
+            
             $requete = "DELETE FROM `utilisateurs` WHERE login = '$this->login'"; 
-
+            
             $query = mysqli_query($db,$requete) ;
-
+            
             var_dump($query) ; 
+            
+            unset($this->login) ;
+            unset($this->password) ; 
+            unset($this->email) ; 
+            unset($this->firstname) ;  
+            unset($this->lastname) ; 
 
             echo 'Votre compte à été supprimer' ; 
         }
+
+        public function update($login,$password,$email,$firstname,$lastname){
+            $db = mysqli_connect("localhost","root","","classes") ; 
+
+            $requete = "UPDATE utilisateurs 
+                            SET login = '$login',
+                                password = '$password',
+                                email = '$email',
+                                firstname = '$firstname',
+                                lastname = '$lastname'
+                                    WHERE login = '$this->login' ;"
+            ;
+
+            $query = mysqli_query($db,$requete) ; 
+            var_dump($query) ; 
+
+            if($query)
+            {
+                echo 'Changement effectuer ! ' ; 
+            }
+            else{
+                echo 'Erreur' ; 
+            }
+        }
     }
 
-    $user1 = new User("bapt","mdp","baptiste.gauthier@laplateforme.io","Baptiste","GAUTHIER") ; 
+    $user1 = new User("bapt", "mdp", "baptiste.gauthier@gmail.com", "Baptiste", "GAUTHIER") ; 
 
     $user2 = new User("JOJO","pass","jojo@gmail.com","Giorno","Giovanna") ; 
 
     $user3 = new User("Charmickael" , "intersecret" , "chuck@buymore.com","Chuck" , "BARTOWSKI") ; 
 
+    $user4 = new User("Colonel", "cia", "john@casey.fr","John","Casey") ; 
+
 
     
 
-    // $user1->register(); 
-    // $user2->register(); 
-    // $user3->register(); 
+    //$user1->update("bapt","mdp","baptiste.gauthier@gmail.com","Baptiste","GAUTHIER"); 
+    $user1->connect(); 
+    //$user4->disconnect(); 
 
-    // $user1->connect() ; 
+    // $user3->connect() ; 
 
-    // $user2->disconnect();
+    //$user2->connect();
 
     // $user3->delete(); 
-
+    //$user1->update("BM", "buymore", "morgan@grimes.fr", "Morgan", "Grims") ; 
 
 ?>
